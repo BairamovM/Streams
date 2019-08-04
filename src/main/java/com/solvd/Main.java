c3935d1dac905c4619381acb0de5630b1d2c7cf2
Neuromancer <muslimbairamov@gmail.com>
2019-08-04 17:03:30 +0300

    First commit

---
+++ src/main/java/com/solvd/Main.java
@@ -0,0 +1,15 @@
+package com.solvd;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    private static final Logger log = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
