f692762ed5449d1277544edf270ccec9877593bd
Neuromancer <muslimbairamov@gmail.com>
2019-08-04 17:39:43 +0300

    change Main with stream

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -1,5 +1,10 @@
 package com.solvd;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.*;
+
 import org.apache.log4j.Logger;
 
 public class Main {
@@ -8,7 +13,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        List<Integer> number = Arrays.asList(4,3,2,1);
+        List<String> names = Arrays.asList("Tree","Book","Stream", "Sony", "Goods" , "Screen");
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
 
+
+        List<Integer> square = number.stream().map(x -> x*x).collect(Collectors.toList());
+        log.info(square);
+
+        List<String> result = names.stream().filter(s->s.startsWith("S")).collect(Collectors.toList());
+        log.info(result);
+
+        List<String> show = names.stream().sorted().collect(Collectors.toList());
+        log.info(show);
+
+        Set<Integer> squareSet = numbers.stream().map(x->x*x).collect(Collectors.toSet());
+        log.info(squareSet);
+
+        number.stream().map(x->x*x).forEach(y->log.info(y));
 
     }
 
