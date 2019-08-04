58b35c5508b4322ee9fb178fe241d8def0ce51b5
Neuromancer <muslimbairamov@gmail.com>
2019-08-04 17:54:52 +0300

    Added class

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -3,6 +3,7 @@ package com.solvd;
 import java.util.List;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.Collection;
 import java.util.stream.*;
 
 import org.apache.log4j.Logger;
@@ -17,6 +18,15 @@ public class Main {
         List<String> names = Arrays.asList("Tree","Book","Stream", "Sony", "Goods" , "Screen");
         List<Integer> numbers = Arrays.asList(1,2,3,4,5);
 
+        Collection<AgePeople> sport = Arrays.asList(
+                new AgePeople("Mike", 15),
+                new AgePeople("Dan", 23),
+                new AgePeople("Dan", 34),
+                new AgePeople(null, 20),
+                new AgePeople("Tom", 30),
+                new AgePeople("Veronika", 19)
+        );
+
 
         List<Integer> square = number.stream().map(x -> x*x).collect(Collectors.toList());
         log.info(square);
@@ -30,7 +40,11 @@ public class Main {
         Set<Integer> squareSet = numbers.stream().map(x->x*x).collect(Collectors.toSet());
         log.info(squareSet);
 
-        number.stream().map(x->x*x).forEach(y->log.info(y));
+        String name = sport.stream().max((p1,p2) -> p1.getAge().compareTo(p2.getAge())).get().getName();
+        log.info("Name = "+name);
+
+        long countName =  sport.stream().filter((p) -> p.getName() != null && p.getName().equals("Dan")).count();
+        log.info("countName = " + countName);
 
     }
 
